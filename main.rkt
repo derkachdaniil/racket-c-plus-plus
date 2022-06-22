ca804645af92f8cf9aee594d2b9fc1d0b74655b0
mimoanon <commandos-daniil@yandex.ru>
2022-06-22 12:57:20 +0300

    Add files via upload

--- main.rkt
+++ main.rkt
@@ -1,3 +1,4 @@
+;        for (int j = 1; j < 10344; j = j + 2) create error with unknown location
 #lang racket/base
 
 (module+ test
@@ -6,7 +7,17 @@
 (module+ test
   (check-equal? (+ 2 2) 4))
 
-(module+ main
+(module+ reader
   (require "lang/reader.rkt")
-  (provide read-syntax))
+  (require "lang/buttons.rkt")
+  ;(require "lang/indenter.rkt")
+  (provide read-syntax get-info)
+  (define (get-info port src-mod src-line src-col src-pos)
+    (define (handle-query key default)
+      (case key
+        [(color-lexer) color-rpp]
+        #;[(drracket:indentation) indent-rpp]
+        [(drracket:toolbar-buttons) button-list]
+        [else default]))
+    handle-query))
   
